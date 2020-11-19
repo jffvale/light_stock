6ae2f31737cc64e23645de3d242d07b836754597
jffvale <jffvale1@gmail.com>
2020-11-19 13:36:39 -0300

    Spring boot zip file opened

---
+++ src/main/java/br/com/masterserv/ligth_stock/LigthStockApplication.java
@@ -0,0 +1,13 @@
+package br.com.masterserv.ligth_stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigthStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LigthStockApplication.class, args);
+	}
+
+}
